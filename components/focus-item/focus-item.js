7cfbc11021ec87ae8d70b76f7fc13c587454162b
Zhihao Lei <463529650@qq.com>
2019-05-13 16:58:34 +0800

    FEAT: add focus-item component

---
+++ components/focus-item/focus-item.js
@@ -0,0 +1,82 @@
+// components/focus-item/focus-item.js
+const focusStateEnum = require('./focus-state-enum')
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    userId: {
+      type: Number || String
+    },
+    uid: {
+      type: Number || String
+    },
+    imageSrc: {
+      type: String,
+      value: "https://gxtdlm-1258532084.cos.ap-beijing.myqcloud.com/image/9a6eab5e22f742d128aa65c377a8e849be31e53d5cee2-W8V9c5_fw658.jpeg"
+    },
+    nickname: {
+      type: String,
+      value: '---'
+    },
+    education: {
+      type: String,
+      value: ''
+    },
+    school: {
+      type: String,
+      value: '--大学'
+    },
+    hometownCity: {
+      type: String,
+      value: ''
+    },
+    hometownProvince: {
+      type: String,
+      value: ''
+    },
+    liveCity: {
+      type: String,
+      value: ''
+    },
+    liveProvince: {
+      type: String,
+      value: ''
+    },
+    height: {
+      type: Number || String,
+      value: '--'
+    },
+    gender: {
+      type: String,
+      value: '--'
+    },
+    age: {
+      type: String || Number,
+      value: '--'
+    },
+    constellation: {
+      type: String,
+      value: '--'
+    },
+    focusStatus: {
+      type: Number,
+      value: focusStateEnum.IS_FOCUS
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    focusStateEnum
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/focus-item/focus-state-enum.js
@@ -0,0 +1,7 @@
+const focusStateEnum = {
+  IS_FOCUS: 0,
+  NOT_FOCUS: 1,
+  BOTH_FOCUS: 2
+}
+
+module.exports = focusStateEnum
