0b11ccd2299c2edbd0aaa61877bababe5867d676
zhangxuelong <zhangxuelong@zhangxuelongdeMacBook-Pro.local>
2019-05-14 12:02:47 +0800

    分享和复制

--- pages/user-info/user-info.js
+++ pages/user-info/user-info.js
@@ -2,7 +2,7 @@
 const app = getApp();
 
 const util = require('../../utils/util')
-
+var that
 Page({
 
   /**
@@ -27,60 +27,34 @@ Page({
     idealTa: '--',
     imageUrl: [],
     constellation: '',
-    wechat:''//个人联系方式微信
+    wechat: '' //个人联系方式微信
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    that = this
     app.loginResolve(() => {
-      let id = options.id
-      let userId = app.globalData.selfUserId
-      this.setData({
-        userId: userId
-      })
-      if (id !== undefined) {
+        let id = options.id
+        let userId = app.globalData.selfUserId
         this.setData({
-          id: id
+          userId: userId
         })
-      }
-      this.getDetail(userId, id)
-    }),
-      wx.showShareMenu({
-        withShareTicket: true //分享之后在其他用户点击后，要求小程序返回分享目标信息
-      })
+        if (id !== undefined) {
+          this.setData({
+            id: id
+          })
+        }
+        this.getDetail(userId, id)
+    }), 
+    wx.showShareMenu({
+      withShareTicket: true //分享之后在其他用户点击后，要求小程序返回分享目标信息
+    })
   },
-  /**分享信息逻辑 */
-  onShareAppMessage: function (res) {
-    var that = this;
-    var userId = that.data.userId;
-    var id = that.data.id;
-    if (res.from === 'button') {
-      console.log("分享的来源是页面内转发")
-    }
-    return {
-      title: '大学生恋爱菌嘉宾信息分享',
-      path: '/pages/user-info/user-info?id=' + id + '&userId=' + userId,
-      success: function (res) {
-        wx.showToast({
-          title: '转发成功',
-          //icon: 'success',
-          duration: 1000
-        })
-      },
-      fail: function (res) {
-        // 转发失败
-        wx.showToast({
-          title: '转发失败',
-        })
-      }
-    }
-  },
+ 
 
   /**复制微信逻辑 ，点击复制微信button获取*/
   copyWechat: function(e) {
-    var that = this;
     var wechat = that.data.wechat
     wx.setClipboardData({
       data: wechat,
@@ -126,7 +100,7 @@ Page({
           constellation: data.aries,
           height: data.height,
           isSelf: data.self,
-          wechat:data.wechat
+          wechat: data.wechat
         })
       })
   },
@@ -142,7 +116,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    var that = this
+      console.log("that.data.id is "+that.data.id)
   },
 
   /**
@@ -172,11 +147,20 @@ Page({
   onReachBottom: function() {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
+  /**分享信息逻辑，右上角的转发 */
+  onShareAppMessage: (res) => {
+    var id = that.data.id
+    var imageUrl0 = that.data.imageUrl[0]
+    if (res.from === 'button') {
+      console.log("来自页面内转发按钮");
+      console.log(res.target);
+    } else {
+      console.log("来自右上角转发菜单")
+    }
+    return {
+      title: '大学生恋爱菌嘉宾信息分享',
+      path: '/pages/user-info/user-info?id=' + id,
+      imageUrl: imageUrl0,
+    }
+  },
 })
