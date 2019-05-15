1f909c38cbb510747298a31ce8b339149436291a
zhangxuelong <zhangxuelong@zhangxuelongdeMacBook-Pro.local>
2019-05-15 22:02:43 +0800

    静态页面和收藏功能完成

---
+++ pages/cooperate/cooperate.js
@@ -0,0 +1,77 @@
+// pages/cooperate/cooperate.js
+var that;
+var app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    telephone:'',
+    wechat:'',
+    qq:'',
+    email:''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    that = this;
+    that.setData({
+      telephone:app.globalData.componeyInfo.telephone,
+      wechat:app.globalData.componeyInfo.wechat,
+      qq:app.globalData.componeyInfo.qq,
+      email:app.globalData.componeyInfo.email
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
